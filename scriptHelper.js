9df6020dc5d1e16ddbeae287449c0d9ce9eaffc7
Harmony Love <harmonylove427@gmail.com>
2023-04-26 22:33:50 -0500

    finalized formSubmission  and validateInput

--- scriptHelper.js
+++ scriptHelper.js
@@ -32,30 +32,14 @@ function validateInput(testInput) {
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
 
-    console.log("$$$", pilot, copilot, fuelLevel, cargoLevel)
+    // console.log("$$$", pilot, copilot, fuelLevel, cargoLevel)
     
     if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty"|| validateInput(cargoLevel) === "Empty") {
                 alert("All fields are required!");
     } else if (validateInput(pilot) === "Is a Number" || validateInput(copilot) === "Is a Number" || validateInput(fuelLevel) === "Not a Number"|| validateInput(cargoLevel) === "Not a Number"){
         alert("Make sure to enter valid information for each field!")
-    } else {
+    } else { 
 
-    }
-
-
-
-     
-    //  console.log("***", statusCheck.value)
-     
-    //     if (validateInput(statusCheck.value)=== "Not a Number"){
-    //         box.style.visibility = visible;   
-             
-    //         statusCheck.style.color = "green";
-    //     } else {
-    //         statusCheck.style.color = "red";
-    //     }
-
-     
         let statusCheck = document.getElementById("launchStatus");
         let box = document.getElementById("faultyItems");
         let pilotStatus = document.getElementById("pilotStatus");
@@ -63,38 +47,32 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
         let fuelStatus = document.getElementById("fuelStatus");
         let cargoStatus = document.getElementById("cargoStatus");
 
-        box.style.visibility += 'visible';  
-        pilotStatus.innerHTML = 'text'
-        //let fuel = document.querySelector("input[name=fuelLevel]").value;
-        console.log("poop", validateInput(fuelLevel))
-        if (formSubmission(fuel)  < 10000){
-        
-        fuelStatus += "There is not enough fuel for the journey.";
-        }
-    //  pStatus.innerText = pilot;
-    
-    //  let pilotStatus = "ready for launch" 
-    //       console.log(`Pilot ${pilot} is ${pilotStatus}.`)
-    //  let copilotStatus = "ready for launch" 
-    //       console.log(`Co-pilot ${copilot} is ${copilotStatus}.`)
-    // //let fuelStatus = "high enough for launch" 
-    //       console.log(` ${fuelStatus}.`)
-    // if (cargoMass > 10000){
-    //    // faultyItems visible and message updates to cargo status too much mass for shuttle to launch 
-    // } else {
-    //     return (``)
-    // }
-    //       console.log(`${cargoStatus}.`)
-    
+        box.style.visibility = 'visible';
+        statusCheck.innerHTML = "Shuttle is ready for launch";
+        statusCheck.style.color = "green";
+        pilotStatus.innerHTML = `Pilot ${pilot} is ready`;
+        copilotStatus.innerHTML = `Co-Pilot ${copilot} is ready`;
 
+        
+        if (fuelLevel < 10000){
+            statusCheck.innerHTML = "Shuttle not ready for launch";
+            statusCheck.style.color = "red";
+        fuelStatus.innerHTML = "There is not enough fuel for the journey";
+        } else {
+            fuelStatus.innerHTML = "Fuel level high enough for launch"
+        }
+        if (cargoLevel > 10000){
+            statusCheck.innerHTML = "Shuttle not ready for launch";
+            statusCheck.style.color = "red";
+        cargoStatus.innerHTML = "There is too much mass for the shuttle to take off";
+        } else {
+            cargoStatus.innerHTML = "Cargo mass low enough for launch"
+        }
+    }
+    
 
 }
   
-//    }
-//    if (document.getElementById("faultyItems").innerHTML === coPilot){
-//     update document.getElementById("copilotStatus") += coPilot;
-//     console.log(coPilot)
-//    }
 async function myFetch() {
     let planetsReturned;
 
@@ -110,9 +88,6 @@ function pickPlanet(planets) {
 }
 
 
-
-//document.getElementById("faultyItems").innerHTML
-//update launchStatus as info is entered on the form
 
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
